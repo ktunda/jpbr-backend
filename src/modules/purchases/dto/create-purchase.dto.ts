01f0eba7897a63691d94e718f413a6a00725c7b1
Thiago <thiagoktunda@gmail.com>
2026-01-23 16:51:44 -0300

    feat: add DTO validation and global validation pipe

--- src/modules/purchases/dto/create-purchase.dto.ts
+++ src/modules/purchases/dto/create-purchase.dto.ts
@@ -1,7 +1,20 @@
+import { IsString, IsNumber, IsOptional } from 'class-validator';
+
 export class CreatePurchaseDto {
+  @IsString()
   storeName: string;
+
+  @IsOptional()
+  @IsString()
   productUrl?: string;
+
+  @IsOptional()
+  @IsString()
   productTitle?: string;
+
+  @IsNumber()
   declaredValueJpy: number;
+
+  @IsString()
   jpTrackingCode: string;
 }
